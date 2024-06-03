72de8aa817fc1effc8e0ea7b02f4042137841b9b
batistalucasss <152137337+lucasdeobatista25@users.noreply.github.com>
2024-06-03 16:12:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function alertaLogin(){
+botão.addEventListener("submit", fnAlerta)
+
+function fnAlerta(){
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+        mostrarPopup(email, senha);
+    });
     
+    function mostrarPopup(email, senha) {
+        alert(`Email: ${email}\nSenha: ${senha}`);
+    }
 }
